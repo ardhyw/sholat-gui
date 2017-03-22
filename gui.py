473f98693d49df0ad74b8ab8157d96df74003b15
Ardhy W. Nugroho <ardhyw@yandex.com>
2017-03-22 20:41:04 +0700

    error handling on empty text and change preset event

--- gui.py
+++ gui.py
@@ -8,12 +8,14 @@ from kivy.clock import Clock
 
 class RootWidget(BoxLayout):
     presets = {
-            'Aceh':{0.6, 117},
-            'Jakarta':{0.6, 117},
-            'Semarang':{0.6, 117},
-            'Surabaya':{0.6, 117},
-            'Makassar':{0.6, 117}
+            'Aceh':(0.6, 117),
+            'Jakarta':(0.6, 117),
+            'Semarang':(0.6, 117),
+            'Surabaya':(0.6, 117),
+            'Makassar':(0.6, 117),
+            'Depok':(-6.3949, 106.7982)
             }
+
     def __init__(self, **kw):
         super(RootWidget, self).__init__(**kw)
         self.ids['preset'].values = sorted(self.presets.keys())
@@ -34,28 +36,34 @@ class RootWidget(BoxLayout):
             pt.tune(offset)
 
         # dapatkan waktu sholat
-        times = pt.getTimes(
-                date.today(),
-                (float(self.lgt.text), float(self.lat.text)),
-                int(self.timezone.text)
-                )
+        if self.lgt.text and self.lat.text:
+            times = pt.getTimes(
+                    date.today(),
+                    (float(self.lgt.text), float(self.lat.text)),
+                    int(self.timezone.text)
+                    )
+            # clear all children
+            self.perhitungan.clear_widgets()
+            self.perhitungan.add_widget(Label(text='Imsyak'))
+            self.perhitungan.add_widget(Label(text='Subuh'))
+            self.perhitungan.add_widget(Label(text='Terbit'))
+            self.perhitungan.add_widget(Label(text='Dzuhur'))
+            self.perhitungan.add_widget(Label(text='Ashar'))
+            self.perhitungan.add_widget(Label(text='Maghrib'))
+            self.perhitungan.add_widget(Label(text='Isya'))
+            self.perhitungan.add_widget(Label(text=times['imsak']))
+            self.perhitungan.add_widget(Label(text=times['fajr']))
+            self.perhitungan.add_widget(Label(text=times['sunrise']))
+            self.perhitungan.add_widget(Label(text=times['dhuhr']))
+            self.perhitungan.add_widget(Label(text=times['asr']))
+            self.perhitungan.add_widget(Label(text=times['maghrib']))
+            self.perhitungan.add_widget(Label(text=times['isha']))
 
-        # clear all children
-        self.perhitungan.clear_widgets()
-        self.perhitungan.add_widget(Label(text='Imsyak'))
-        self.perhitungan.add_widget(Label(text='Subuh'))
-        self.perhitungan.add_widget(Label(text='Terbit'))
-        self.perhitungan.add_widget(Label(text='Dzuhur'))
-        self.perhitungan.add_widget(Label(text='Ashar'))
-        self.perhitungan.add_widget(Label(text='Maghrib'))
-        self.perhitungan.add_widget(Label(text='Isya'))
-        self.perhitungan.add_widget(Label(text=times['imsak']))
-        self.perhitungan.add_widget(Label(text=times['fajr']))
-        self.perhitungan.add_widget(Label(text=times['sunrise']))
-        self.perhitungan.add_widget(Label(text=times['dhuhr']))
-        self.perhitungan.add_widget(Label(text=times['asr']))
-        self.perhitungan.add_widget(Label(text=times['maghrib']))
-        self.perhitungan.add_widget(Label(text=times['isha']))
+    def do_preset(self):
+        pos = self.presets[self.preset.text]
+        self.lgt.text = str(pos[0])
+        self.lat.text = str(pos[1])
+        self.do_hisab()
 
 class Gui(App):
     def build(self):
