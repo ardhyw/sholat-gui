52802baac7d4362ffce0abfd505974c3cb6f663d
Ardhy W. Nugroho <ardhyw@yandex.com>
2017-03-22 14:36:28 +0700

    refine layout

--- gui.py
+++ gui.py
@@ -4,7 +4,16 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 
 class RootWidget(BoxLayout):
-    pass
+    presets = {
+            'Aceh':{0.6, 117},
+            'Jakarta':{0.6, 117},
+            'Semarang':{0.6, 117},
+            'Surabaya':{0.6, 117},
+            'Makassar':{0.6, 117}
+            }
+    def __init__(self, **kw):
+        super(RootWidget, self).__init__(**kw)
+        self.ids['preset'].values = sorted(self.presets.keys())
 
 class Gui(App):
     def build(self):
